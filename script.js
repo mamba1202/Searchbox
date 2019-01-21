e974ea39e403e2a8d518e52e45c60a499ea4ba9b
mambazhang <1612786059@qq.com>
2019-01-21 14:32:23 +0800

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+keyword.oninput = function(e){
+    var value = keyword.value
+    if(value){
+        suggestion.classList.add('active')
+    }else{
+        suggestion.classList.remove('active')
+    }
+}
